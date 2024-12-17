0a7f6ef6d5b3f3d1977a96dfd4e0c1d2212ee4a0
iiTAi <kimagureowata@icloud.com>
2024-12-17 14:02:58 +0900

    add: harvest method on WaveAnalyzer

--- src/hum2midi/WaveAnalyzer.py
+++ src/hum2midi/WaveAnalyzer.py
@@ -11,34 +11,47 @@ class WaveAnalyzer:
 
     Attributes:
         wave_path (str): 解析する音声データのパス
+        method (str): 解析手法の指定
+        
     """
 
-    def __init__(self, wave_path: str):
+    def __init__(self, wave_path: str, method: str = "harvest"):
         """コンストラクタ
 
         Args:
             wave_path (str): 解析する音声データのパス
+            method (str): 解析手法の指定. "harvest"か"dio"を指定. Defaults to "harvest".
         """
         
         self.wave_path = wave_path
+        self.method = method
 
     def get_pitch(self) -> Tuple[np.ndarray, float]:
         """ピッチを取得するメソッド
 
-        pyworldで、dioとstonemaskを使ってピッチを取得する
+        音声データからピッチを取得する
+        pyworldの、harvestかdioのどちらかの手法を指定する
 
         Returns:
-            Tuple[np.ndarray, float]: ピッチのリストと時間軸
+            Tuple[np.ndarray, float]: ピッチのリストと時間軸の間隔
+
+        Note:
+            ステレオ音源の場合は、平均値を振幅としたモノラル音源に変換される
         """
         
         fs, data = wav.read(self.wave_path)
-
         data = data.astype(np.float64)
         if data.ndim >= 2:
             data = np.mean(data, axis=1)
         
-        _f0_list, timeaxis = pw.dio(data, fs)
-        f0_list = pw.stonemask(data, _f0_list, timeaxis, fs)
+        if self.method == "dio":
+            _f0_list, timeaxis = pw.dio(data, fs)
+            f0_list = pw.stonemask(data, _f0_list, timeaxis, fs)
+        elif self.method == "harvest":
+            f0_list, timeaxis = pw.harvest(data, fs)
+        else:
+            raise ValueError("method must be 'dio' or 'harvest'")
+
         BIAS = 10
         f0_list += BIAS
         time = timeaxis[1] - timeaxis[0]
