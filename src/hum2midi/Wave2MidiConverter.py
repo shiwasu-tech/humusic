1337089db2fb6cc9589ec06ccc913843336ebfc5
iiTAi <kimagureowata@icloud.com>
2024-12-17 17:52:06 +0900

    edit: class initialization

--- src/hum2midi/Wave2MidiConverter.py
+++ src/hum2midi/Wave2MidiConverter.py
@@ -1,5 +1,6 @@
 import numpy as np
 from mido import MidiFile, MidiTrack, Message, MetaMessage, bpm2tempo
+from WaveAnalyzer import WaveAnalyzer
 
 class Wave2MidiConverter:
     """録音した音声データをMIDIデータに変換するクラス
@@ -9,77 +10,84 @@ class Wave2MidiConverter:
     隣接するノート番号が連続している場合、同じノート番号の長さを加算する
 
     Attributes:
+        wave_path (str): 解析する音声データのパス
+        method (str): 解析手法の指定. "harvest"か"dio"を指定
+        output_path (str): 保存するMIDIファイルのパス
+        f0_list (np.ndarray): ピッチのリスト
+        time (float): ピッチの時間軸の間隔
         bpm (float): BPM
-        velocity (int): ノートオン時のベロシティ
-        output_path (str): 保存するMIDIファイル
-        midi (MidiFile): MIDIファイルオブジェクト
-        track (MidiTrack): MIDIトラックオブジェクト
+        midi (MidiFile): MIDIファイル
+        track (MidiTrack): MIDIトラック
     """
 
     def __init__(
             self,
-            bpm: float,
-            velocity: int = 100,
+            wave_path: str,
+            method: str = "harvest",
             output_path: str = "resources/output.mid"
     ):
         """コンストラクタ
 
+        初期化の際、WaveAnalyzerを用いてピッチとBPMを取得する
+
         Args:
-            bpm (float): BPM
-            velocity (int, optional): 音量. Defaults to 80.
+            wave_path (str): 解析する音声データのパス
+            method (str, optional): 解析手法の指定. "harvest"か"dio"を指定. Defaults to "harvest".
             output_path (str, optional): 保存するMIDIファイルのパス. Defaults to "resources/output.mid".
         """
-
-        self.bpm = bpm
-        self.velocity = velocity
+        print("Initializing Wave2MidiConverter...")
+        self.wave_path = wave_path
+        self.method = method
         self.output_path = output_path
+
+        analyzer = WaveAnalyzer(self.wave_path, self.method)
+        f0_list, time, bpm = analyzer.analyze_pitch_and_bpm()
+        self.f0_list = f0_list
+        self.time = time
+        self.bpm = bpm
 
         self.midi = MidiFile()
         self.track = MidiTrack()
         self.__track_setup()
+        print("Wave2MidiConverter initialized")
 
         
     def __track_setup(self):
         """MIDIトラックの設定を行うメソッド"""
-
         self.midi.tracks.append(self.track)
         self.track.append(MetaMessage('set_tempo', tempo=bpm2tempo(self.bpm)))
 
 
-    def create_midi(self, f0_list: np.ndarray, time: float):
+    def create_midi(self, velocity: int = 100):
         """MIDIデータを作成するメソッド
 
         Args:
-            f0_list (np.ndarray): ピッチのリスト
-            time (float): ピッチの時間軸
+            velocity (int, optional): ノートのベロシティ. Defaults to 100.
         """
-
-        notes = self._make_notes(f0_list, time)
+        print("Creating MIDI data...")
+        notes = self._make_notes()
         joined_notes = self._join_notes(notes)
-        self._joined_notes_to_midi(joined_notes)
+        self._joined_notes_to_midi(joined_notes, velocity)
 
 
-    def _make_notes(self, f0_list: np.ndarray, time: float) -> list:
-        """ピッチのリストからノート番号のリストを作成するメソッド
+    def _make_notes(self) -> list:
+        """ノート番号のリストを作成するメソッド
 
-        Args:
-            f0_list (np.ndarray): ピッチのリスト
-            time (float): ピッチの時間軸
+        ピッチをノート番号に変換することで、ノート番号のリストを作成する
 
         Returns:
             list: ノート番号のリスト
         """
-
         start_point = 0
-        for s, f0 in enumerate(f0_list):
+        for s, f0 in enumerate(self.f0_list):
             if f0 != 0:
                 start_point = s
                 break
 
-        FRAME = int((7.5 / self.bpm) * (1 / time))
+        FRAME = int((7.5 / self.bpm) * (1 / self.time))
         separated_f0_list = np.array_split(
-            f0_list[start_point:],
-            len(f0_list[start_point:]) // FRAME
+            self.f0_list[start_point:],
+            len(self.f0_list[start_point:]) // FRAME
         )
 
         notes = []
@@ -89,7 +97,8 @@ class Wave2MidiConverter:
         return notes
 
 
-    def __f0_to_note_number(self, f0: float) -> int:
+    @staticmethod
+    def __f0_to_note_number(f0: float) -> int:
         """ピッチをノート番号に変換するメソッド
 
         無音の場合はノート番号を-1とする
@@ -100,14 +109,13 @@ class Wave2MidiConverter:
         Returns:
             int: ノート番号
         """
-        
         if f0 == 0:
             return -1
         else:
             return int(69 + 12 * np.log2(f0 / 440))
 
-
-    def _join_notes(self, notes: list) -> list:
+    @staticmethod
+    def _join_notes(notes: list) -> list:
         """同じノード番号の連続した音符を結合するメソッド
 
         Args:
@@ -116,7 +124,6 @@ class Wave2MidiConverter:
         Returns:
             list: 結合されたノート番号の辞書型リスト{ノート番号, 長さ}
         """
-
         joined_notes = []
         prev_note = -1
 
@@ -130,30 +137,29 @@ class Wave2MidiConverter:
         return joined_notes
 
 
-    def _joined_notes_to_midi(self, joined_notes: list):
+    def _joined_notes_to_midi(self, joined_notes: list, velocity: int):
         """結合されたノート番号のリストをMIDIデータに変換するメソッド
 
         MIDIデータはMIDIファイルとして保存される
 
         Args:
             joined_notes (list): 結合されたノート番号の辞書型リスト{ノート番号, 長さ}
+            velocity (int): ノートのベロシティ
         """
-
         for note in joined_notes:
             if note["note"] != -1:
                 self.track.append(Message(
                     "note_on",
                     note=note["note"],
-                    velocity=self.velocity,
+                    velocity=velocity,
                     time=0
                 ))
                 self.track.append(Message(
                     "note_off",
                     note=note["note"],
-                    velocity=self.velocity,
+                    velocity=velocity,
                     time=note["length"]*60
                 ))
-
             else:
                 self.track.append(Message(
                     "note_off",
@@ -164,3 +170,23 @@ class Wave2MidiConverter:
 
         self.midi.save(self.output_path)
         print(f"Saved as {self.output_path}")
+
+
+    def set_wave_path(self, wave_path: str):
+        """音声データのパスを設定するメソッド
+
+        音声データに依存する情報も再取得する
+
+        Args:
+            wave_path (str): 音声データのパス
+        """
+        self.wave_path = wave_path
+        analyzer = WaveAnalyzer(self.wave_path)
+        f0_list, time, bpm = analyzer.analyze_pitch_and_bpm()
+        self.f0_list = f0_list
+        self.time = time
+        self.bpm = bpm
+
+if __name__ == "__main__":
+    wave2midi = Wave2MidiConverter("resources/output.wav")
+    wave2midi.create_midi()
